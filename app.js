fc26f4a959645e194357734e9d3feddfdbcbb604
trinhngockhang <20168267@student.hust.edu.vn>
2017-04-25 21:16:58 +0700

    update get update delete

--- app.js
+++ app.js
@@ -1,63 +1,36 @@
-  console.log('Hello nodemon');
+console.log('Hello nodemon');
 
-  const fs = require('fs');
-  //dung cai thu vien express
-  const express = require('express');
+const fs = require('fs');
+//dung cai thu vien express
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-  const imagesController =require(__dirname + '/modules/images/imagesController');
-  const bodyParser=require('body-parser');
-  var app = express();
+const config = require('./config.json');
+const imagesRouter = require(__dirname + '/modules/api/images/');
 
-  //set public folder public
-  //app.use(urlencoded)
-  app.use(express.static(__dirname + '/public'));
-  app.use(bodyParser.json({extended:true}));
-  app.use(bodyParser.urlencoded({extended:true}));
+var app = express();
 
-  app.get('/', (req, res) => {
-    res.send('./public/index.html');
-  })
+//set public folder public
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({ extended : true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-  app.post('/image', (req, res) => {
-    //doc du lieu tu file  imageData
-   var imageInfoCollection= imagesController.fetchImageCollection();
+app.get('/', (req, res) => {
+  res.send('./public/index.html');
+})
 
-    //khai bao object
-    var imageInfo = {
-      name : req.body.name,
-      imageLink : req.body.imageLink,
-      description : req.body.description
-    }
+app.use('/api/image', imagesRouter);
 
-    console.log('post data',req.body);
-    //push data moi vao collection
-    imageInfoCollection.push(imageInfo);
+mongoose.connect(config.connectionString, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected db success');
+  }
+})
 
-    //luu lai vao file
-    imagesController.saveImageCollection(imageInfoCollection);
-    //bao thanh cong
-    res.send('Success');
-  })
-
-  app.get('/image', (req,res) => {
-    var imageInfoCollection= imagesController.fetchImageCollection();
-
-    var htmlString='';
-
-    imageInfoCollection.forEach((data) =>{
-      htmlString += `<div>${data.name}</div><img src ="${data.imageLink}"><div>${data.description}</div>`;
-    })
-    res.send(htmlString);
-  })
-
-  app.put('/image',(req,res) =>{
-
-  })
-
-  app.delete('/image',(req,res)=>{
-
-  })
-  //mo 1 cai port de chay local
-  app.listen(6969, (req, res) => {
-    console.log('app listen on 6969');
-  })
+//mo 1 cai port de chay local
+app.listen(config.port, (req, res) => {
+  console.log(`app listen on ${config.port}`);
+})
